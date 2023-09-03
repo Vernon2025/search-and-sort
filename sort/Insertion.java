112add9e78b2f907e5ab883ee87ca73114f8193f
Vernon2025 <143050322+Vernon2025@users.noreply.github.com>
2023-09-03 12:56:07 -0700

    Insertion Complete

--- sort/Insertion.java
+++ sort/Insertion.java
@@ -16,10 +16,17 @@ public class Insertion
      */
     public static int[] sort(int[] arr) {
         // Your algorithm goes here!
-        int first = 0;
-        int last = arr.length;
-        
-        Arrays.sort(arr,first,last);
+        int n = arr.length;
+        for (int i = 1; i < n; i++) {
+            int k = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > k) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = k;
+        }
         return arr;
     }
 
