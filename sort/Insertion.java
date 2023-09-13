7046316322a3c8e4f6474cb9fc405382131cdfb7
Vernon2025 <143050322+Vernon2025@users.noreply.github.com>
2023-09-12 20:02:36 -0700

    Update Insertion.java

--- sort/Insertion.java
+++ sort/Insertion.java
@@ -16,19 +16,26 @@ public class Insertion
      */
     public static int[] sort(int[] arr) {
         // Your algorithm goes here!
-        int n = arr.length;
+        int n = arr.length;  // Get the length of the input array.
+    
+        // Start iterating through the array from the second element (index 1).
         for (int i = 1; i < n; i++) {
-            int k = arr[i];
-            int j = i - 1;
+            int k = arr[i];  // Store the current element in 'k'.
+            int j = i - 1;   // Initialize 'j' to the index before 'i'.
+    
+            // This inner loop is used to find the correct position for 'k'
+            // within the sorted part of the array.
             while (j >= 0 && arr[j] > k) {
-                arr[j + 1] = arr[j];
-                j--;
+                arr[j + 1] = arr[j];  // Shift the larger element one position to the right.
+                j--;                 // Move 'j' leftwards in the sorted part of the array.
             }
-            
-            arr[j + 1] = k;
+    
+            arr[j + 1] = k;  // Insert 'k' in its correct sorted position.
         }
-        return arr;
+        
+        return arr;  // Return the sorted array.
     }
+    
 
     public static void main(String[] args) {
         int[] arr = {53,85,93,25,39,27,42,5,24,45,33,51,5,80,4,7,91,
